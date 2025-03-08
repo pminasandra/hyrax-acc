f06bcf7ebcc29698bfdf4e4eabc6771d9bd44ef7
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2025-03-08 11:22:31 +0100

    Successfully reading hyrac accelerometer data

--- config.py
+++ config.py
@@ -11,8 +11,11 @@ import os.path
 PROJECTROOT = open(".cw", "r").read().rstrip()
 DATA = os.path.join(PROJECTROOT, "Data")
 FIGURES = os.path.join(PROJECTROOT, "Figures")
+ACC_DIR = os.path.join(DATA, "ACC")
 
 formats=['png', 'pdf', 'svg']
 
+
 #Miscellaneous
+ACC_FILE_SEP = ";"
 SUPPRESS_INFORMATIVE_PRINT = False
